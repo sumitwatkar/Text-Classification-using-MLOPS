3f1830fcc2cc57d5c0730ab4b2d65178b405e8c3
Sumit Watkar <watkar.sumit@gmail.com>
2024-08-14 12:06:23 +0530

    model signature tests fix

--- tests/model_testing.py
+++ tests/model_testing.py
@@ -42,7 +42,7 @@ class TestModelLoading(unittest.TestCase):
         return latest_version[0].version if latest_version else None
 
     def test_model_loaded_properly(self):
-        self.assertIsNotNone(self.new_model)
+        self.assertIsNotNone(self.model)
 
     def test_model_signature(self):
         # Create a dummy input for the model based on expected input shape
